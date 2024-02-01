6f487bfb745d2357fe2d34059b300cb56e67467d
Plamen Mitev <44363976+o0pmitev@users.noreply.github.com>
2024-02-01 17:37:36 +0200

    fix: add static method

--- utils/helpers.mjs
+++ utils/helpers.mjs
@@ -8,4 +8,17 @@ export default class CustomHelpers {
       console.log(args)
   }
 
+  /**
+  * @description Gets the coordinates of the mouse click on a given element (x, y)
+  * @param {string} elementClass class name of the element
+  */
+  static getElementClickOriginPoint(elementClass) {
+    document.querySelector(`.${elementClass}`).onclick = function(e) {
+      var rect = e.target.getBoundingClientRect();
+      var x = e.clientX - rect.left; // x position within the element.
+      var y = e.clientY - rect.top; // y position within the element.
+      console.log(x, y);
+    }
+  }
+
 }
